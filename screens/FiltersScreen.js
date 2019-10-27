ced112725b4a62d88944d5c2aed630969b839cb6
Yuniarto Pamungkas <yuniarto.pamungkas345@gmail.com>
2019-10-27 22:50:37 +0700

    Finished

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,14 +1,72 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native'
+import { Switch } from 'react-native-paper'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import HeaderButton from '../components/HeaderButton'
+import Colors from '../constants/Colors'
+
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.label}</Text>
+            <Switch
+                trackColor={{ true: Colors.primaryColor }}
+                thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+                value={props.state}
+                onValueChange={props.onChange}
+            />
+        </View>
+    )
+}
 
 
 const FiltersScreen = (props) => {
+    const { navigation } = props;
+
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+
+    const saveFilters = useCallback(() => {
+        const appliedFilters = {
+          glutenFree: isGlutenFree,
+          lactoseFree: isLactoseFree,
+          vegan: isVegan,
+          isVegetarian: isVegetarian
+        };
+    
+        console.log(appliedFilters);
+      }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+    
+      useEffect(() => {
+        navigation.setParams({ save: saveFilters });
+      }, [saveFilters]);
+
     return (
         <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
+            <Text style={styles.title}>Available Filters</Text>
+            <FilterSwitch
+                label='Is Gluten Free'
+                state={isGlutenFree}
+                onChange={newValue => setIsGlutenFree(newValue)}
+            />
+            <FilterSwitch
+                label='Lactose Free'
+                state={isLactoseFree}
+                onChange={newValue => setIsLactoseFree(newValue)}
+            />
+            <FilterSwitch
+                label='Vegan'
+                state={isVegan}
+                onChange={newValue => setIsVegan(newValue)}
+            />
+            <FilterSwitch
+                label='Vegetarian'
+                state={isVegetarian}
+                onChange={newValue => setIsVegetarian(newValue)}
+            />
         </View>
     )
 }
@@ -16,15 +74,29 @@ const FiltersScreen = (props) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
+        // justifyContent: 'center',
         alignItems: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        marginVertical: 15
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        margin: 20,
+        fontSize: 20,
+        textAlign: 'center'
+
     }
 })
 
 FiltersScreen.navigationOptions = (navigationData) => {
     return {
         headerTitle: 'Filter Meals',
-        headerLeft:
+        headerLeft: (
             <HeaderButtons HeaderButtonComponent={HeaderButton} >
                 <Item title='menu' iconName='ios-menu'
                     onPress={() => {
@@ -32,10 +104,15 @@ FiltersScreen.navigationOptions = (navigationData) => {
                     }}
                 />
             </HeaderButtons>
-
-
+        ),
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton} >
+                <Item title='save' iconName='ios-save'
+                    onPress={navigationData.navigation.getParam('save')}
+                />
+            </HeaderButtons>
+        ),
     }
-
 }
 
 export default FiltersScreen;
